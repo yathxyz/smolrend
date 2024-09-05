ca7b1853c41c0dde34086d63eec04322827517e7
Ioannis Eleftheriou <me@yath.xyz>
2024-09-05 18:09:14 +0100

    Vulkan tutorial base code

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,39 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    glfwInit();
+class HelloTriangleApplication {
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+public:
+  void run() {
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+private:
+  void initVulkan() {
+  }
 
-    std::cout << extensionCount << " extensions supported\n";
+  void mainLoop() {
+  }
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+  void cleanup() {
+  }
+};
 
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
+int main(void) {
+  HelloTriangleApplication app;
 
-    glfwDestroyWindow(window);
+  try {
+    app.run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    glfwTerminate();
 
-    return 0;
+  return EXIT_SUCCESS;
 }
