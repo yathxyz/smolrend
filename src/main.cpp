444bc31e277a547ac0daa99b1e9b9d2f127e73eb
Ioannis Eleftheriou <me@yath.xyz>
2024-09-05 19:29:49 +0100

    Window add attempt

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -8,20 +10,40 @@ class HelloTriangleApplication {
 
 public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
-  }
+  };
 
 private:
+
+  const uint32_t WIDTH = 800;
+  const uint32_t HEIGHT = 600;
+
+  GLFWwindow* window; // This will break an assertion
+
+  void initWindow() {
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+   GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  };
+
   void initVulkan() {
-  }
+  };
 
   void mainLoop() {
-  }
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+
+    };
+  };
 
   void cleanup() {
-  }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  };
 };
 
 int main(void) {
@@ -32,7 +54,7 @@ int main(void) {
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
-  }
+  };
 
 
   return EXIT_SUCCESS;
