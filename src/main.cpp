8fe9e2890966ac1a30e8536fd97145253762b4e1
Ioannis Eleftheriou <iielefth@amazon.com>
2024-09-09 17:29:33 +0100

    Add comment and reformat

--- src/main.cpp
+++ src/main.cpp
@@ -10,40 +10,50 @@ const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
 
 private:
-    GLFWwindow* window;
+  GLFWwindow* window;
 
-    void initWindow() { // Holy shit this is so ugly. All functions act as side effects?
-        glfwInit();
+  void initWindow() { // Holy shit this is so ugly. All functions act as side effects?
+      glfwInit();
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+      glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+      glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    }
+      window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
 
-    void initVulkan() {
 
-    }
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
+  void initVulkan() {
+  /* From what I understand, Vulkan works by initialising an instance.
+   *
+   * The instance means the connection between the application and the library itself.
+   * I'm afraid I don't quite understand if the tutorial by that means dynamic.
+   *
+   * I don't think it even matters if it's static or dynamic really. It's still runtime.
+   *
+   * We need to fill out detailed information before initialising. NB what happens.
+   * */
+  }
 
-    void cleanup() {
-        glfwDestroyWindow(window);
+  void mainLoop() {
+      while (!glfwWindowShouldClose(window)) {
+          glfwPollEvents();
+      }
+  }
 
-        glfwTerminate();
-    }
+  void cleanup() {
+      glfwDestroyWindow(window);
+
+      glfwTerminate();
+  }
 };
 
 int main() {
