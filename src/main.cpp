0a37498c290adbbcc08dfd5c0c0c7d2cf0b27c17
Ioannis Eleftheriou <iielefth@amazon.com>
2024-09-09 18:59:54 +0100

    Add vulkan init details

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ public:
 
 private:
   GLFWwindow* window;
+  VkInstance instance;
 
   void initWindow() { // Holy shit this is so ugly. All functions act as side effects?
       glfwInit();
@@ -29,6 +30,48 @@ private:
       window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
   }
 
+  void createInstance() {
+
+    /*
+     * In Vulkan it's common to pass structs as arguments to functions.
+     * This is uhhhhh... uuuuuhhhhhhhhhhhhh
+     * */
+
+    // Optional
+    VkApplicationInfo appInfo{}; // Squiggly brackets probably denote a struct. Not an object.
+
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan Triangle Tutorial";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine"; // Wait, there are optimisations for this?
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+    // bunch of metadata it seems.
+
+
+    // We actually need this
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    // I feel like I can really simplify this
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    // You don't need to do it like this. It's all freaking side effects anyway
+    // vkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    // Start barfing
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("faled to create instance.");
+    }
+  }
 
 
   void initVulkan() {
@@ -40,7 +83,10 @@ private:
    * I don't think it even matters if it's static or dynamic really. It's still runtime.
    *
    * We need to fill out detailed information before initialising. NB what happens.
+   *
+   * See createInstance
    * */
+
   }
 
   void mainLoop() {
